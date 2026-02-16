44d8077aa7b758bc6e2bb5773d0a94ebc94904e0
Deniz Çelik <denizcelik2002@gmail.com>
2026-02-16 15:56:05 +0300

    next-auth working finally

--- auth.ts
+++ auth.ts
@@ -1,19 +1,9 @@
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 
-const users = [
-  { id: "1", name: "Admin", email: "admin@test.com", password: "password" },
-  { id: "2", name: "Test User", email: "test@test.com", password: "1234" }
-]
-
-export const auth = NextAuth({
+export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [
     Credentials({
-      name: "Credentials",
-      credentials: {
-        email: {},
-        password: {}
-      },
       async authorize(credentials) {
         if (
           credentials?.email === "admin@test.com" &&
@@ -22,9 +12,9 @@ export const auth = NextAuth({
           return { id: "1", name: "Admin", email: "admin@test.com" }
         }
         return null
-      }
-    })
+      },
+    }),
   ],
   session: { strategy: "jwt" },
-  secret: process.env.NEXTAUTH_SECRET
+  secret: process.env.NEXTAUTH_SECRET,
 })
